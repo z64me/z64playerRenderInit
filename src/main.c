adb589cf1cf1ec91ad3aeff2704c41ab1fb15afe
z64me <45853441+z64me@users.noreply.github.com>
2021-02-02 23:32:45 -0500

    Update main.c

--- src/main.c
+++ src/main.c
@@ -1,28 +1,71 @@
 #include <z64ovl/oot/debug.h>
 #include <z64ovl/helpers.h>
-#include "oot-debug.h" /* TODO remove oot-debug; rely only on z64ovl */
 
-void main(void)
+#define OverrideLimbDrawOpa void*
+#define PostLimbDrawOpa void*
+
+/* the function in gameplay_keep overwrites the arrow texture */
+#define GK_ARROW 0x04004380
+
+typedef void fptr(void *);
+
+typedef void fptr_8008F470(
+	z64_global_t *globalCtx
+	, void **skeleton
+	, vec3s_t *jointTable
+	, int32_t dListCount
+	, int32_t lod
+	, int32_t tunic
+	, int32_t boots
+	, int32_t face
+	, OverrideLimbDrawOpa overrideLimbDraw
+	, PostLimbDrawOpa postLimbDraw
+	, void *data
+);
+
+/* moved to gameplay_keep to make porting easier */
+#if 0
+asm("func_8008F470 = 0x80141030");
+extern void func_8008F470(
+	z64_global_t *globalCtx
+	, void **skeleton
+	, vec3s_t *jointTable
+	, int32_t dListCount
+	, int32_t lod
+	, int32_t tunic
+	, int32_t boots
+	, int32_t face
+	, OverrideLimbDrawOpa overrideLimbDraw
+	, PostLimbDrawOpa postLimbDraw
+	, void *data
+);
+#endif
+
+void main_wowProc(
+	z64_global_t *globalCtx
+	, void **skeleton
+	, vec3s_t *jointTable
+	, int32_t dListCount
+	, int32_t lod
+	, int32_t tunic
+	, int32_t boots
+	, int32_t face
+	, OverrideLimbDrawOpa overrideLimbDraw
+	, PostLimbDrawOpa postLimbDraw
+	, void *data)
 {
-	short playerobj;
-	int form = *(int*)(0x8015E660 + 4); /* TODO make game/version agnostic */
-	playerobj = g_playerobjdep[form];
-	
-	int slot = z_object_slot(&g_object_context, playerobj);
-	if (slot < 0)
-		return;
-	
-	unsigned char *zobj = g_object_context.slot[slot].data;
-	if (!zobj)
-		return;
+	unsigned char *zobj = (void*)zh_seg2ram(0x06000000);
 	unsigned end = *(unsigned*)(zobj + (0x5000 + 0x800 - 4));
+	unsigned *has_relocd;
+	fptr_8008F470 *playerDraw;
+	fptr *exec;
 	
 	/* zobj doesn't contain embedded overlay */
 	if (!end)
-		return;
+		goto L_next;
 	
 	/* do relocations */
-	unsigned *has_relocd = (unsigned*)(zobj + (0x5000 + 0x800 - 8));
+	has_relocd = (unsigned*)(zobj + (0x5000 + 0x800 - 8));
 	if (!*has_relocd)
 	{
 		unsigned ofs = *(unsigned*)(zobj + (end - 4));
@@ -40,7 +83,23 @@ void main(void)
 		);
 		*has_relocd = start;
 	}
-	typedef void fptr(void *);
-	fptr *exec = (fptr*)(*has_relocd);
+	exec = (fptr*)(*has_relocd);
 	exec(zobj);
+	
+	/* draw player model */
+L_next:
+	playerDraw = (fptr_8008F470*)zh_seg2ram(GK_ARROW);
+	playerDraw(
+		globalCtx
+		, skeleton
+		, jointTable
+		, dListCount
+		, lod
+		, tunic
+		, boots
+		, face
+		, overrideLimbDraw
+		, postLimbDraw
+		, data
+	);
 }
