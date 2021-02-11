c50415e398d30f0bba7fa42cdd1339bbab97ab0f
z64me <45853441+z64me@users.noreply.github.com>
2021-02-11 15:57:51 -0500

    cleanup and readability

--- src/main.c
+++ src/main.c
@@ -7,34 +7,24 @@
 #endif
 #include <z64ovl/helpers.h>
 
-#define OverrideLimbDrawOpa void*
-#define PostLimbDrawOpa void*
-
-struct ovlHead
+struct z64OvlHead
 {
-	uint32_t text; /* size of .text section */
+	uint32_t textSz;   /* size of .text section   */
+	uint32_t dataSz;   /* size of .data section   */
+	uint32_t rodataSz; /* size of .rodata section */
+	uint32_t bssSz;    /* size of .bss section    */
 };
 
-typedef void fptr(void *);
-
-#ifdef RENDERINIT_OOT_10U
-  asm("func_8008F470 = 0x80079F48");
-#else
-  asm("func_8008F470 = 0x8008F470");
-#endif
-extern void func_8008F470(
-	z64_global_t *globalCtx
-	, void **skeleton
-	, vec3s_t *jointTable
-	, int32_t dListCount
-	, int32_t lod
-	, int32_t tunic
-	, int32_t boots
-	, int32_t face
-	, OverrideLimbDrawOpa overrideLimbDraw
-	, PostLimbDrawOpa postLimbDraw
-	, void *data
-);
+struct z64playerRenderInit
+{
+   /* offsets are relative to the start of Link's model in ram */
+   uint32_t     start;           /* offset of start of overlay    */
+   union
+   {
+      uint32_t  header;          /* offset of overlay header      */
+      void      (*exec)(void*);  /* main routine after relocating */
+   } u;
+};
 
 void main_wowProc(
 	z64_global_t *globalCtx
@@ -45,56 +35,48 @@ void main_wowProc(
 	, int32_t tunic
 	, int32_t boots
 	, int32_t face
-	, OverrideLimbDrawOpa overrideLimbDraw
-	, PostLimbDrawOpa postLimbDraw
+	, void *overrideLimbDraw
+	, void *postLimbDraw
 	, void *data
 )
 {
-	uint8_t *zobj = (void*)zh_seg2ram(0x06000000);
-	uintptr_t *ptr = (uintptr_t*)(zobj + (0x5000 + 0x800 - 4));
-	struct ovlHead *header;
+	uint8_t *model = (void*)zh_seg2ram(0x06000000);
+	struct z64playerRenderInit *rinit = (void*)(model + 0x57f8);
+	struct z64OvlHead *header;
 	void *start;
-	fptr *exec;
 	uint32_t size;
 	
-	/* `ptr` points to 0x57FC in Link's ZOBJ, which will be:
-	 *  -> zero, if no embedded overlay is present
-	 *  -> or the offset of the overlay's header within `zobj`
-	 *     (the u32 preceding this u32 is overlay start offset)
-	 *  -> or a pointer to the overlay's main routine
-	 */
-	
-	/* zobj doesn't contain embedded overlay */
-	if (!*ptr)
-		goto L_next;
-	
-	/* not yet relocated */
-	if (!(*ptr & 0x80000000))
+	/* model contains embedded overlay */
+	if (rinit->u.header)
 	{
-		/* relocate overlay */
-		header = (struct ovlHead*)(zobj + *ptr);
-		start = zobj + *(ptr-1);
-		z_overlay_do_relocation(
-			start
-			, header
-			, (void*)0x80800000
-		);
+		/* not yet relocated (not a ram address) */
+		if (!(rinit->u.header & 0x80000000))
+		{
+			/* relocate overlay from virtual ram to physical ram */
+			header = (struct z64OvlHead*)(model + rinit->u.header);
+			start = model + rinit->start;
+			z_overlay_do_relocation(
+				start
+				, header
+				, (void*)0x80800000
+			);
+			
+			/* update `exec` to point to main routine */
+			rinit->u.exec = start;
+			
+			/* clear instruction cache for .text section memory region */
+			size = header->textSz;
+			osWritebackDCache(start, size);
+			osInvalICache(start, size);
+		}
 		
-		/* update `ptr` to main routine */
-		*ptr = (uintptr_t)start;
-		
-		/* clear instruction cache for .text section memory region */
-		size = header->text;
-		osWritebackDCache(start, size);
-		osInvalICache(start, size);
+		/* run main routine */
+		rinit->u.exec(model);
 	}
-	exec = (fptr*)(*ptr);
-	exec(zobj);
 	
 	/* draw player model */
-L_next:
-	func_8008F470(
-		(void*)GLOBAL_CONTEXT/*globalCtx*/
+	z_player_lib_draw_link(
+		(void*)GLOBAL_CONTEXT
 		, skeleton
 		, jointTable
 		, dListCount
